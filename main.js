717b5662e2585aa9df9f3fe97854cc5ea5ba52fd
vizhukova <veronikazhukova@gmail.com>
2015-11-28 11:48:32 +0200

    some changes

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var app = angular.module('FirstApplication',[]);
 //angular.module('FirstApplication')
 app.controller('MainController',function($scope){
     $scope.activeTab = 0;
+    $scope.newComment = '';
+
+
   $scope.product  ={
       id:1,
       description:"bla bla",
@@ -18,4 +21,39 @@ app.controller('MainController',function($scope){
           }
       ]
   }
+
+    $scope.postComment = function() {
+
+        $service("product", function($q, $timeout) {
+            var def = $q.deffered();
+
+            $.timeout(function(){
+                def.resolve(data)
+            }, 2000)
+
+            return def.promise()
+        })
+
+        $service.then(function() {
+            if($scope.newComment == '') {
+                alert('Comment is empty!')
+                return
+            }
+            $scope.product.comments.push({
+                author: 'newauthor',
+                message:$scope.newComment
+            })
+        }, function() {})
+
+    }
+/*
+    if($scope.newComment == '') {
+        alert('Comment is empty!')
+        return
+    }
+    $scope.product.comments.push({
+        author: 'newauthor',
+        message:$scope.newComment
+    })
+    */
 })
